f86d32c21262f8605088f445d7c9ba6f6409953f
AnthonyT007 <atrisano@my.waketech.edu>
2025-04-10 09:57:08 -0400

    Create gradebook.js

---
+++ gradebook.js
@@ -0,0 +1,10 @@
+function fetchGradeData() {
+     console.log("Fetching grade data...");
+
+
+function populateGradeBook(data) {
+    console.log("Populating gradebook with data:", data);
+
+const gradedata = fetchGradeData();
+populateGradebook(gradeData);
+  
